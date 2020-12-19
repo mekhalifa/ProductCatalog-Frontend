f7f2e6fe7f9aa53083558e6b93e65408e1137daa
Mohamed Khalifa <m_e_khalifa@hotmail.com>
2020-12-19 17:54:24 +0200

    add excel

--- src/app/components/product/list-product/list-product.component.ts
+++ src/app/components/product/list-product/list-product.component.ts
@@ -2,8 +2,9 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { DeleteModelComponent } from './../delete-model/delete-model.component';
 import { ProductService } from './../../../services/product.service';
 import { Product } from './../../../models/product';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-list-product',
@@ -11,6 +12,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./list-product.component.css']
 })
 export class ListProductComponent implements OnInit {
+
+  @ViewChild('TABLE', { static: false }) TABLE!: ElementRef;
 
   // products list
   public productList!: Product[];
@@ -68,6 +71,14 @@ export class ListProductComponent implements OnInit {
   onSubmit(formData: FormGroup): void{
     this.searchKey = formData.value.searchKey;
     this.loadProducts();
+  }
+
+
+  ExportTOExcel(): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.TABLE.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, 'ScoreSheet.xlsx');
   }
 
 }
